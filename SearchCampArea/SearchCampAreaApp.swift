409f05c1f6e202f50d282627957bcc47d48bd4f5
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2022-05-23 20:41:19 +0300

    Initial Commit

---
+++ SearchCampArea/SearchCampAreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchCampAreaApp.swift
+//  SearchCampArea
+//
+//  Created by İbrahim Erdogan on 23.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SearchCampAreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
